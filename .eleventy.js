84cdeb271f06268c5b28f9f6aba58ff3ba143768
Diego <diegolive5000@gmail.com>
2022-08-01 00:19:30 +0200

    NAA-48 build a functioning and responsive navbar for all resolutions

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/');
   eleventyConfig.addPassthroughCopy('src/css/');
+  eleventyConfig.addPassthroughCopy('src/js/');
 
   eleventyConfig.addWatchTarget('src/css/');
+  eleventyConfig.addWatchTarget('src/js/');
 
   return {
     dir: {
